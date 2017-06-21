dd16e080f73c536050e6dfd536b59a2379338e40
ben awad <benawad97@gmail.com>
2017-06-20 20:27:59 -0500

    Not quite working yet

--- kit/lib/apollo.js
+++ kit/lib/apollo.js
@@ -2,27 +2,35 @@
 // IMPORTS
 
 // Apollo client library
-import { createNetworkInterface, ApolloClient } from 'react-apollo';
+import { ApolloClient } from 'react-apollo';
+import { PersistedQueryNetworkInterface } from 'persistgraphql';
 
 // Custom configuration/settings
 import { APOLLO } from 'config/project';
 
+import queryMap from './extractedQueries';
 // ----------------------
 
 // Create a new Apollo network interface, to point to our API server.
 // Note:  By default in this kit, we'll connect to a sample endpoint that
 // repsonds with simple messages.  Update [root]/config.js as needed.
-const networkInterface = createNetworkInterface({
+const networkInterface = new PersistedQueryNetworkInterface({
+  queryMap,
   uri: APOLLO.uri,
 });
 
 // Helper function to create a new Apollo client, by merging in
 // passed options alongside the defaults
 function createClient(opt = {}) {
-  return new ApolloClient(Object.assign({
-    reduxRootSelector: state => state.apollo,
-    networkInterface,
-  }, opt));
+  return new ApolloClient(
+    Object.assign(
+      {
+        reduxRootSelector: state => state.apollo,
+        networkInterface,
+      },
+      opt,
+    ),
+  );
 }
 
 // Creates a new browser client
